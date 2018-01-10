f680928c3ba57ef29f481e8e298bdf607e863039
Mingyi Zhang <zhangmingyi@kedacom.com>
2018-01-10 17:22:15 +0800

    .

--- c++/build.bat
+++ c++/build.bat
@@ -1,9 +1,15 @@
-set BUILDDIR=%~dp0\plugin\build
-if not exist %BUILDDIR% (
-   mkdir %BUILDDIR%
-)
-pushd %BUILDDIR%
-dir
-cmake -G "Visual Studio 14 2015 Win64" ..
+set __DIR=%~dp0
+
+REM build plugin
+pushd %__DIR%\addon
+
+call node-gyp configure --msvs_version=2015
+call node-gyp build
+copy build\Release\node_plugin.node ..\..\bin\windows-x64.node
+popd
+
+pushd %__DIR%\plugin
+cmake -G "Visual Studio 14 2015 Win64" .
 cmake --build . --target ALL_BUILD --config Release
+copy  Release\example.dll ..\..\bin\example.dll
 popd
---
+++ index.js
@@ -0,0 +1,5 @@
+node_plugin=require('./bin/windows-x64')
+Plugin = node_plugin.Plugin
+console.log(Plugin);
+
+module.exports =Plugin;
